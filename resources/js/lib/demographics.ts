015589c89b26fc05132fd82022057453813ae8de
Maximilian Schöll <max@schoell.co>
2025-03-22 20:49:56 +0000

    feat: add prefer_not_to_say

--- resources/js/lib/demographics.ts
+++ resources/js/lib/demographics.ts
@@ -24,14 +24,8 @@ export const mapYearToAgeGroup = (year: string) => {
     if (age < 65) return '55_to_64';
     return '65_plus';
 };
-
 const DEMOGRAPHIC_FIELDS = [
-    {
-        label: 'Geburtsjahr',
-        key: 'birthyear',
-        hidden: false,
-        options: generateYearOptions(),
-    },
+    { label: 'Geburtsjahr', key: 'birthyear', hidden: false, options: generateYearOptions() },
     {
         label: 'Alter',
         key: 'age_group',
@@ -43,9 +37,22 @@ const DEMOGRAPHIC_FIELDS = [
         key: 'gender',
         hidden: false,
         options: [
-            { value: 'male', label: 'Männlich' },
-            { value: 'female', label: 'Weiblich' },
-            { value: 'other', label: 'Divers' },
+            {
+                value: 'male',
+                label: 'Männlich',
+            },
+            {
+                value: 'female',
+                label: 'Weiblich',
+            },
+            {
+                value: 'other',
+                label: 'Divers',
+            },
+            {
+                value: 'prefer_not_to_say',
+                label: 'Keine Angabe',
+            },
         ],
     },
     {
@@ -53,11 +60,30 @@ const DEMOGRAPHIC_FIELDS = [
         key: 'marital_status',
         hidden: false,
         options: [
-            { value: 'single', label: 'Ledig' },
-            { value: 'married_or_civil_union', label: 'Verheiratet oder in eingetragener Lebenspartnerschaft' },
-            { value: 'separated', label: 'In Trennung lebend' },
-            { value: 'divorced', label: 'Geschieden' },
-            { value: 'widowed', label: 'Verwitwet' },
+            {
+                value: 'single',
+                label: 'Ledig',
+            },
+            {
+                value: 'married_or_civil_union',
+                label: 'Verheiratet oder in eingetragener Lebenspartnerschaft',
+            },
+            {
+                value: 'separated',
+                label: 'In Trennung lebend',
+            },
+            {
+                value: 'divorced',
+                label: 'Geschieden',
+            },
+            {
+                value: 'widowed',
+                label: 'Verwitwet',
+            },
+            {
+                value: 'prefer_not_to_say',
+                label: 'Keine Angabe',
+            },
         ],
     },
     {
@@ -65,14 +91,42 @@ const DEMOGRAPHIC_FIELDS = [
         key: 'education',
         hidden: false,
         options: [
-            { value: 'no_degree', label: 'Kein Schulabschluss' },
-            { value: 'primary', label: 'Hauptschulabschluss' },
-            { value: 'secondary', label: 'Realschulabschluss / Mittlere Reife' },
-            { value: 'vocational', label: 'Fachhochschulreife' },
-            { value: 'abitur', label: 'Abitur / Allgemeine Hochschulreife' },
-            { value: 'bachelor', label: 'Bachelor-Abschluss' },
-            { value: 'master', label: 'Master-Abschluss' },
-            { value: 'doctorate', label: 'Promotion / Doktortitel' },
+            {
+                value: 'no_degree',
+                label: 'Kein Schulabschluss',
+            },
+            {
+                value: 'primary',
+                label: 'Hauptschulabschluss',
+            },
+            {
+                value: 'secondary',
+                label: 'Realschulabschluss / Mittlere Reife',
+            },
+            {
+                value: 'vocational',
+                label: 'Fachhochschulreife',
+            },
+            {
+                value: 'abitur',
+                label: 'Abitur / Allgemeine Hochschulreife',
+            },
+            {
+                value: 'bachelor',
+                label: 'Bachelor-Abschluss',
+            },
+            {
+                value: 'master',
+                label: 'Master-Abschluss',
+            },
+            {
+                value: 'doctorate',
+                label: 'Promotion / Doktortitel',
+            },
+            {
+                value: 'prefer_not_to_say',
+                label: 'Keine Angabe',
+            },
         ],
     },
     {
@@ -80,19 +134,62 @@ const DEMOGRAPHIC_FIELDS = [
         key: 'current_activity',
         hidden: false,
         options: [
-            { value: 'attending_school', label: 'Besuch einer allgemeinbildenden Schule' },
-            { value: 'studying', label: 'Studium' },
-            { value: 'vocational_training', label: 'Berufliche Ausbildung' },
-            { value: 'retraining', label: 'Umschulung' },
-            { value: 'voluntary_military_service', label: 'Freiwilliger Wehrdienst' },
-            { value: 'bfd_fsj_fej', label: 'Bundesfreiwilligendienst, freiwilliges soziales oder ökologisches Jahr' },
-            { value: 'career_break', label: 'Erwerbsunterbrechung (z. B. Pflegezeit, Mutterschutz, Elternzeit ohne Teilzeit-Tätigkeit)' },
-            { value: 'employed', label: 'Erwerbstätig (Vollzeit, Teilzeit oder geringfügig)' },
-            { value: 'retired', label: 'Rentner*in / Pensionär*in' },
-            { value: 'unemployed', label: 'Arbeitslos' },
-            { value: 'permanently_unfit', label: 'Dauerhaft erwerbsunfähig' },
-            { value: 'household_management', label: 'Hausfrau/Hausmann' },
-            { value: 'other', label: 'Sonstige' },
+            {
+                value: 'attending_school',
+                label: 'Besuch einer allgemeinbildenden Schule',
+            },
+            {
+                value: 'studying',
+                label: 'Studium',
+            },
+            {
+                value: 'vocational_training',
+                label: 'Berufliche Ausbildung',
+            },
+            {
+                value: 'retraining',
+                label: 'Umschulung',
+            },
+            {
+                value: 'voluntary_military_service',
+                label: 'Freiwilliger Wehrdienst',
+            },
+            {
+                value: 'bfd_fsj_fej',
+                label: 'Bundesfreiwilligendienst, freiwilliges soziales oder ökologisches Jahr',
+            },
+            {
+                value: 'career_break',
+                label: 'Erwerbsunterbrechung (z. B. Pflegezeit, Mutterschutz, Elternzeit ohne Teilzeit-Tätigkeit)',
+            },
+            {
+                value: 'employed',
+                label: 'Erwerbstätig (Vollzeit, Teilzeit oder geringfügig)',
+            },
+            {
+                value: 'retired',
+                label: 'Rentner*in / Pensionär*in',
+            },
+            {
+                value: 'unemployed',
+                label: 'Arbeitslos',
+            },
+            {
+                value: 'permanently_unfit',
+                label: 'Dauerhaft erwerbsunfähig',
+            },
+            {
+                value: 'household_management',
+                label: 'Hausfrau/Hausmann',
+            },
+            {
+                value: 'other',
+                label: 'Sonstige',
+            },
+            {
+                value: 'prefer_not_to_say',
+                label: 'Keine Angabe',
+            },
         ],
     },
     {
@@ -100,16 +197,50 @@ const DEMOGRAPHIC_FIELDS = [
         key: 'household_size',
         hidden: false,
         options: [
-            { value: '1', label: '1 Person' },
-            { value: '2', label: '2 Personen' },
-            { value: '3', label: '3 Personen' },
-            { value: '4', label: '4 Personen' },
-            { value: '5', label: '5 Personen' },
-            { value: '6', label: '6 Personen' },
-            { value: '7', label: '7 Personen' },
-            { value: '8', label: '8 Personen' },
-            { value: '9', label: '9 Personen' },
-            { value: '10_or_more', label: '10 oder mehr Personen' },
+            {
+                value: '1',
+                label: '1 Person',
+            },
+            {
+                value: '2',
+                label: '2 Personen',
+            },
+            {
+                value: '3',
+                label: '3 Personen',
+            },
+            {
+                value: '4',
+                label: '4 Personen',
+            },
+            {
+                value: '5',
+                label: '5 Personen',
+            },
+            {
+                value: '6',
+                label: '6 Personen',
+            },
+            {
+                value: '7',
+                label: '7 Personen',
+            },
+            {
+                value: '8',
+                label: '8 Personen',
+            },
+            {
+                value: '9',
+                label: '9 Personen',
+            },
+            {
+                value: '10_or_more',
+                label: '10 oder mehr Personen',
+            },
+            {
+                value: 'prefer_not_to_say',
+                label: 'Keine Angabe',
+            },
         ],
     },
     {
@@ -117,22 +248,74 @@ const DEMOGRAPHIC_FIELDS = [
         key: 'federal_state',
         hidden: false,
         options: [
-            { value: 'baden_wuerttemberg', label: 'Baden-Württemberg' },
-            { value: 'bayern', label: 'Bayern' },
-            { value: 'berlin', label: 'Berlin' },
-            { value: 'brandenburg', label: 'Brandenburg' },
-            { value: 'bremen', label: 'Bremen' },
-            { value: 'hamburg', label: 'Hamburg' },
-            { value: 'hessen', label: 'Hessen' },
-            { value: 'mecklenburg_vorpommern', label: 'Mecklenburg-Vorpommern' },
-            { value: 'niedersachsen', label: 'Niedersachsen' },
-            { value: 'nordrhein_westfalen', label: 'Nordrhein-Westfalen' },
-            { value: 'rheinland_pfalz', label: 'Rheinland-Pfalz' },
-            { value: 'saarland', label: 'Saarland' },
-            { value: 'sachsen', label: 'Sachsen' },
-            { value: 'sachsen_anhalt', label: 'Sachsen-Anhalt' },
-            { value: 'schleswig_holstein', label: 'Schleswig-Holstein' },
-            { value: 'thueringen', label: 'Thüringen' },
+            {
+                value: 'baden_wuerttemberg',
+                label: 'Baden-Württemberg',
+            },
+            {
+                value: 'bayern',
+                label: 'Bayern',
+            },
+            {
+                value: 'berlin',
+                label: 'Berlin',
+            },
+            {
+                value: 'brandenburg',
+                label: 'Brandenburg',
+            },
+            {
+                value: 'bremen',
+                label: 'Bremen',
+            },
+            {
+                value: 'hamburg',
+                label: 'Hamburg',
+            },
+            {
+                value: 'hessen',
+                label: 'Hessen',
+            },
+            {
+                value: 'mecklenburg_vorpommern',
+                label: 'Mecklenburg-Vorpommern',
+            },
+            {
+                value: 'niedersachsen',
+                label: 'Niedersachsen',
+            },
+            {
+                value: 'nordrhein_westfalen',
+                label: 'Nordrhein-Westfalen',
+            },
+            {
+                value: 'rheinland_pfalz',
+                label: 'Rheinland-Pfalz',
+            },
+            {
+                value: 'saarland',
+                label: 'Saarland',
+            },
+            {
+                value: 'sachsen',
+                label: 'Sachsen',
+            },
+            {
+                value: 'sachsen_anhalt',
+                label: 'Sachsen-Anhalt',
+            },
+            {
+                value: 'schleswig_holstein',
+                label: 'Schleswig-Holstein',
+            },
+            {
+                value: 'thueringen',
+                label: 'Thüringen',
+            },
+            {
+                value: 'prefer_not_to_say',
+                label: 'Keine Angabe',
+            },
         ],
     },
     {
@@ -140,17 +323,54 @@ const DEMOGRAPHIC_FIELDS = [
         key: 'income',
         hidden: false,
         options: [
-            { value: 'under_1000', label: 'Unter 1000 €' },
-            { value: '1000_1999', label: '1.000 bis 1.999 €' },
-            { value: '2000_2999', label: '2.000 bis 2.999 €' },
-            { value: '3000_3999', label: '3.000 bis 3.999 €' },
-            { value: '4000_4999', label: '4.000 bis 4.999 €' },
-            { value: '5000_5999', label: '5.000 bis 5.999 €' },
-            { value: '6000_6999', label: '6.000 bis 6.999 €' },
-            { value: '7000_7999', label: '7.000 bis 7.999 €' },
-            { value: '8000_8999', label: '8.000 bis 8.999 €' },
-            { value: '9000_9999', label: '9.000 bis 9.999 €' },
-            { value: '10000_plus', label: '10.000 € oder mehr' },
+            {
+                value: 'under_1000',
+                label: 'Unter 1000 €',
+            },
+            {
+                value: '1000_1999',
+                label: '1.000 bis 1.999 €',
+            },
+            {
+                value: '2000_2999',
+                label: '2.000 bis 2.999 €',
+            },
+            {
+                value: '3000_3999',
+                label: '3.000 bis 3.999 €',
+            },
+            {
+                value: '4000_4999',
+                label: '4.000 bis 4.999 €',
+            },
+            {
+                value: '5000_5999',
+                label: '5.000 bis 5.999 €',
+            },
+            {
+                value: '6000_6999',
+                label: '6.000 bis 6.999 €',
+            },
+            {
+                value: '7000_7999',
+                label: '7.000 bis 7.999 €',
+            },
+            {
+                value: '8000_8999',
+                label: '8.000 bis 8.999 €',
+            },
+            {
+                value: '9000_9999',
+                label: '9.000 bis 9.999 €',
+            },
+            {
+                value: '10000_plus',
+                label: '10.000 € oder mehr',
+            },
+            {
+                value: 'prefer_not_to_say',
+                label: 'Keine Angabe',
+            },
         ],
     },
     {
@@ -158,16 +378,37 @@ const DEMOGRAPHIC_FIELDS = [
         key: 'political_affiliation',
         hidden: false,
         options: [
-            { value: 'very_conservative', label: 'Sehr konservativ' },
-            { value: 'conservative', label: 'Konservativ' },
-            { value: 'middle', label: 'Mitte' },
-            { value: 'liberal', label: 'Liberal' },
-            { value: 'very_liberal', label: 'Sehr liberal' },
-            { value: 'no_opinion', label: 'Keine Meinung' },
+            {
+                value: 'very_conservative',
+                label: 'Sehr konservativ',
+            },
+            {
+                value: 'conservative',
+                label: 'Konservativ',
+            },
+            {
+                value: 'middle',
+                label: 'Mitte',
+            },
+            {
+                value: 'liberal',
+                label: 'Liberal',
+            },
+            {
+                value: 'very_liberal',
+                label: 'Sehr liberal',
+            },
+            {
+                value: 'no_opinion',
+                label: 'Keine Meinung',
+            },
+            {
+                value: 'prefer_not_to_say',
+                label: 'Keine Angabe',
+            },
         ],
     },
 ];
-
 export const getDemographicFields = () => {
     return DEMOGRAPHIC_FIELDS.filter((field) => !field.hidden);
 };
