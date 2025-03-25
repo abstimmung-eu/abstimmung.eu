40a134e04c23296d585148a1728906cb624cf76a
Maximilian Schöll <max@schoell.co>
2025-03-25 01:29:33 +0000

    fix: logo

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -1,5 +1,5 @@
 import { CheckCircleIcon } from 'lucide-react';
 
 export default function AppLogoIcon() {
-    return <CheckCircleIcon className="text-blue-899 dark:text-blue-500"></CheckCircleIcon>;
+    return <CheckCircleIcon className="text-blue-900 dark:text-blue-500"></CheckCircleIcon>;
 }
